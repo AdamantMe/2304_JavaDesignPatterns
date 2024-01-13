047486a51a774b9a28f700d448dfb537d45b885a
Alipc598 <117017807+Alipc598@users.noreply.github.com>
2024-01-13 14:37:17 +0100

    IPv4/IPv6 impl.

---
+++ src/main/java/org/example/simulator/network/IPv4.java
@@ -0,0 +1,13 @@
+package org.example.simulator.network;
+
+public class IPv4 extends NetworkProtocol {
+    public IPv4() {
+        super("IPv4", "Stateful");
+    }
+
+    @Override
+    public void configureProtocol() {
+       
+    }
+
+}
---
+++ src/main/java/org/example/simulator/network/IPv6.java
@@ -0,0 +1,13 @@
+package org.example.simulator.network;
+
+public class IPv6 extends NetworkProtocol {
+    public IPv6() {
+        super("IPv6", "Stateless");
+    }
+
+    @Override
+    public void configureProtocol() {
+      
+    }
+
+}
