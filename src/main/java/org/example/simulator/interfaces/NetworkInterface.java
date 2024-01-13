06cc0cf13882f46ccb47a49cc783a594eed4c7d0
Alipc598 <117017807+Alipc598@users.noreply.github.com>
2024-01-13 13:02:52 +0100

    NetworkInterface implementation
    Imported related classes, implementation for them will be pushed later.

--- src/main/java/org/example/simulator/interfaces/NetworkInterface.java
+++ src/main/java/org/example/simulator/interfaces/NetworkInterface.java
@@ -1,4 +1,50 @@
 package org.example.simulator.interfaces;
 
-public interface NetworkInterface {
+public abstract class NetworkInterface {
+    protected String name;
+    protected String ipAddress;
+    protected String mask;
+    protected String connectedIp;
+
+    public NetworkInterface(String name, String ipAddress, String mask) {
+        this.name = name;
+        this.ipAddress = ipAddress;
+        this.mask = mask;
+    }
+
+    // Attributes
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getMask() {
+        return mask;
+    }
+
+    public void setMask(String mask) {
+        this.mask = mask;
+    }
+
+    public String getConnectedIp() {
+        return connectedIp;
+    }
+
+    public void setConnectedIp(String connectedIp) {
+        this.connectedIp = connectedIp;
+    }
+
+    // Abstract method to be implemented by subclasses
+    public abstract void connect(String ipAddress);
 }
