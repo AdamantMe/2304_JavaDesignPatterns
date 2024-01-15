5db9c2c5b6f56536b1a819a265533452c405ceb1
h0nz1n <jan.saradin@praguecollege.cz>
2024-01-15 09:45:42 +0100

    Added logic for routing table in Router.java device

--- src/main/java/org/example/simulator/network/device/Router.java
+++ src/main/java/org/example/simulator/network/device/Router.java
@@ -3,12 +3,17 @@ package org.example.simulator.network.device;
 import org.example.simulator.network.connection.Connection;
 import org.example.simulator.strategy.ActionEvent;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Router implements Device {
     private final String id;
-    private Connection connection; // TODO: add the logic for routing table
+    private Connection connection;
+    private Map<String, Connection> routingTable;
 
     public Router(String id) {
         this.id = id;
+        this.routingTable = new HashMap<>();
     }
 
     @Override
@@ -27,8 +32,13 @@ public class Router implements Device {
     @Override
     public void sendPacket(String destinationId, String data) {
         if (connection != null) {
-            connection.sendPacket(this.id, destinationId, data);
-            System.out.println("Router " + id + " sent packet to " + destinationId + " with data: " + data);
+            Connection destinationConnection = routingTable.get(destinationId);
+            if (destinationConnection != null) {
+                destinationConnection.sendPacket(this.id, destinationId, data);
+                System.out.println("Router " + id + " sent packet to " + destinationId + " with data: " + data);
+            } else {
+                System.out.println("Router " + id + " does not have a route to " + destinationId);
+            }
         } else {
             System.out.println("Router " + id + " has no connection to send packet to " + destinationId);
         }
@@ -37,5 +47,9 @@ public class Router implements Device {
     @Override
     public void connectTo(Connection connection) {
         this.connection = connection;
+    }
+
+    public void addRoute(String destinationId, Connection connection) {
+        routingTable.put(destinationId, connection);
     }
 }
